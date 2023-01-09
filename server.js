f7cdda9f905145e77ab119585427c122706a2a53
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2023-01-09 17:04:03 -0600

    Changed back to just cors options, maybe all that was needed to be removed was withCredentials?

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.use(logger);
 //app.use(credentials);
 
 // Cross Origin Resource Sharing -> put in separate file in config folder.
-//app.use(cors(corsOptions));
-app.use(function(req,res,next) { 
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type, Accept');
-    next();
-})
+app.use(cors(corsOptions));
+// app.use(function(req,res,next) { 
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type, Accept');
+//     next();
+// })
 
 
 // built-in middleware to handle urlencoded data
