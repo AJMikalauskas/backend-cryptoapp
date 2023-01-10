3dc9d0a6cac86fae4cc5127b4259739fab1ca15c
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2023-01-10 08:14:47 -0600

    Changed true boolean to a string instead?

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,8 +5,8 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req,res,next) => {
     const origin = req.headers.origin;
     if(allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Origin', "http://localhost:3002")
+        res.header('Access-Control-Allow-Credentials', 'true');
     }
     next();
 }
