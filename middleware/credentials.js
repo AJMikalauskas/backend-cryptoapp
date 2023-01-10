760e6445c17a5a64ca087f8fae3b6437b942802c
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2023-01-10 08:11:43 -0600

    Added specific header in the credentials file, this may work

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -6,6 +6,7 @@ const credentials = (req,res,next) => {
     const origin = req.headers.origin;
     if(allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', "http://localhost:3002")
     }
     next();
 }
